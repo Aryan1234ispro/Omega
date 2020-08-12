e0c96ab57510e52e8cf6c1e52f07d312511e35ba
Aryan1234ispro <69597778+Aryan1234ispro@users.noreply.github.com>
2020-08-12 23:09:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,3 +20,13 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyAhIwc8VU0l3L4a4oC6DxAXUBWq-D-__0w",
+  authDomain: "hacking-69a99.firebaseapp.com",
+  databaseURL: "https://hacking-69a99.firebaseio.com",
+  projectId: "hacking-69a99",
+  storageBucket: "hacking-69a99.appspot.com",
+  messagingSenderId: "444593237037",
+  appId: "1:444593237037:web:47c359f5e01fe840121ca4",
+  measurementId: "G-ZYMMSJB2E2"
+};
